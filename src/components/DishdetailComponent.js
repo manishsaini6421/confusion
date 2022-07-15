0d5028b2e8c0722e64e3dd69cd0db8fec54b3183
manishsaini6421 <manishsaini6421@gmail.com>
2022-07-15 11:48:53 +0530

    redux-thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,6 +2,8 @@ import React,{Component} from "react";
 import { Card, CardImg, CardText, CardBody, CardTitle ,Breadcrumb,BreadcrumbItem,Row,Modal,Button,ModalHeader,ModalBody,Label,Col} from "reactstrap";
 import {Link} from "react-router-dom";
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
+
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => (val) && (val.length >= len);
@@ -87,7 +89,7 @@ export class CommentForm extends Component {
 }
 
     function RenderDish({dish}) {
-
+         
         if (dish != null) {
             return (
                 <div className='col-12 col-md-5 m-1'>
@@ -149,35 +151,54 @@ export class CommentForm extends Component {
         if (dish == null) {
             return (<div></div>);
         }
+        if (props.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        else if (props.dish != null) {
+            return (
+                <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+    
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row">
+                        <RenderDish dish={props.dish} />
+                   
+                    <div className="col-12 col-md-5 m-1">
+                    <RenderComments comments={props.comments}
+            addComment={props.addComment}
+            dishId={props.dish.id}
+          />
+                        
+                    </div>
+                </div>
+                </div>
+            );
+        }
 
         
-
-        return (
-            <div className="container">
-            <div className="row">
-                <Breadcrumb>
-
-                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                </Breadcrumb>
-                <div className="col-12">
-                    <h3>{props.dish.name}</h3>
-                    <hr />
-                </div>                
-            </div>
-            <div className="row">
-                    <RenderDish dish={props.dish} />
-               
-                <div className="col-12 col-md-5 m-1">
-                <RenderComments comments={props.comments}
-        addComment={props.addComment}
-        dishId={props.dish.id}
-      />
-                    
-                </div>
-            </div>
-            </div>
-        );
     }
 
 
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,16 +1,19 @@
-import { createStore ,combineReducers} from "redux";
+import {createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import { Dishes } from "./dishes";
 import { Comments } from "./comments";
 import { Leaders } from "./leaders";
 import { Promotions } from "./promotions";
 export const ConfigureStore = ()=>{
     const store =createStore(
-    combineReducers({
-        dishes:Dishes,
-        comments:Comments,
-        leaders:Leaders,
-        promotions:Promotions
-    })
+        combineReducers({
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders
+        }),
+        applyMiddleware(thunk, logger)
     );
     return store;
 
