472269ea0ae209ab6eabfd5af5d686aa7d51f8a9
manishsaini6421 <manishsaini6421@gmail.com>
2022-07-19 21:46:39 +0530

    changes in package.json file

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename="/confusion">
         <div>
           <Main />
         </div>
