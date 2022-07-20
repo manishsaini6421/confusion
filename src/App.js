be2bb70bde5cd8ae816a04f91e222a4e5ed98752
manishsaini6421 <manishsaini6421@gmail.com>
2022-07-21 00:28:51 +0530

    change in route

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-      <BrowserRouter basename="/confusion">
+      <BrowserRouter basename="/">
         <div>
           <Main />
         </div>
