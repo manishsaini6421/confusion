87d39f8680647924947ef599984f715ada2e1580
manishsaini6421 <manishsaini6421@gmail.com>
2022-07-20 12:51:38 +0530

    upadeted without server

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -46,7 +46,7 @@ class Header extends Component {
             <NavbarToggler onClick={this.toggleNav} />
             <NavbarBrand className="mr-auto style">
               <img
-                src= "assets/images/logo.png"
+                src= "/assets/images/logo.png"
                 height="30"
                 width="41"
                 alt="Ristorante Con Fusion"
